5fae99760f659d06434d2bc2bbcf106f58f7f2ac
edgar cruz <97191134+DOOMNOTRON@users.noreply.github.com>
2023-03-13 01:46:11 -0700

    Log/Register/Recovery scene complete
    Created the Log/Register/Recovery scene.

---
+++ GameGauntletIsland/Assets/Scripts/LogInPagePlayFab.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LogInPagePlayFab : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI TopText;
+    [SerializeField] TextMeshProUGUI MessageText;
+    
+    [Header("Login")]
+    [SerializeField] TMP_InputField EmailLoginInput;
+    [SerializeField] TMP_InputField PasswordLoginInput;
+    [SerializeField] GameObject LoginPage;
+
+    [Header("Register")]
+    [SerializeField] TMP_InputField UserNameRegisterInput;
+    [SerializeField] TMP_InputField EmailRegisterInput;
+    [SerializeField] TMP_InputField PasswordRegisterInput;
+    [SerializeField] GameObject RegisterPage;
+
+    [Header("Recovery")]
+    [SerializeField] TMP_InputField EmailRecoveryInput;
+    [SerializeField] GameObject RecoveryPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
