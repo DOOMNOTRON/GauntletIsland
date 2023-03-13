54fd60c751d74c6f9d0a194d95047596cdbbf023
edgar cruz <97191134+DOOMNOTRON@users.noreply.github.com>
2023-03-13 09:18:30 -0700

    Created the Node and node graph system for dialogue branches.
    Created the Node and node graph system for dialogue branches. This will allow me to add a dialogue tree system.

---
+++ GameGauntletIsland/Assets/Scripts/DialogueScripts/DialogueGraphView.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+// using Subtegral.DialogueSystem.DataContainers;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
+
+public class DialogueGraphView : GraphView
+{
+    private readonly Vector2 defaultNodeSize = new Vector2(150, 200);
+
+    public DialogueGraphView()
+    {
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new ContentDragger());
+
+        AddElement(GenerateEntryPointNode());
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        var compatiblePorts = new List<Port>();
+        ports.ForEach((port) =>
+        {
+            if (startPort != port && startPort.node != port.node)
+                compatiblePorts.Add(port);
+        });
+        return compatiblePorts;
+        
+    }
+
+    private Port GeneratePort(DialogueNode node, Direction portDirection, Port.Capacity capacity = Port.Capacity.Single)
+    {
+        return node.InstantiatePort(Orientation.Horizontal, portDirection, capacity, typeof(float)); // Type does not matter
+    }
+
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "START",
+            GUID = Guid.NewGuid().ToString(),
+            DialogueText = "ENTRYPOINT",
+            EntryPoint = true
+        };
+
+        var generatedPort = GeneratePort(node, Direction.Output);
+        generatedPort.portName = "Next";
+        node.outputContainer.Add(generatedPort);
+
+        node.RefreshExpandedState();
+        node.RefreshPorts();
+
+        node.SetPosition(new Rect(100, 200, 100, 150));
+        return node;
+    }
+
+    public void CreateNode(string nodeName)
+    {
+        AddElement(CreateDialogueNode(nodeName));
+    }
+
+    public DialogueNode CreateDialogueNode(string nodeName)
+    {
+        var dialogueNode = new DialogueNode
+        {
+            title = nodeName,
+            GUID = Guid.NewGuid().ToString()
+        };
+
+        var inputPort = GeneratePort(dialogueNode, Direction.Input, Port.Capacity.Multi);
+        inputPort.portName = "Input";
+        dialogueNode.inputContainer.Add(inputPort);
+
+        //var button = new Button(clickEvent () => {AddChoicePort})
+        dialogueNode.RefreshExpandedState();
+        dialogueNode.RefreshPorts();
+        dialogueNode.SetPosition(new Rect(Vector2.zero, defaultNodeSize));
+
+        return dialogueNode;
+    }
+}
